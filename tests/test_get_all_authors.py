6c06390de22a917cbed885b73706c15e9f9b2c40
Fernando Angelin <fernando_angelin@hotmail.com>
2024-07-13 19:13:22 +0100

    chore: add response length validation

--- tests/test_get_all_authors.py
+++ tests/test_get_all_authors.py
@@ -7,6 +7,7 @@ def test_get_all_authors():
     response = requests.get(f"{BASE_URL}{AUTHOR_ENDPOINT}")
     assert response.status_code == 200
     authors_dict = response.json()
+    assert len(authors_dict) > 0
     assert isinstance(authors_dict, dict)
     assert "authors" in authors_dict
     assert isinstance(authors_dict["authors"], list)
