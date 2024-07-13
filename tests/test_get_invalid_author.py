b4e36cd041a6368107ef3c0cb08b689d969e9640
Fernando Angelin <fernando_angelin@hotmail.com>
2024-07-13 18:31:39 +0100

    test: get invalid author

---
+++ tests/test_get_invalid_author.py
@@ -0,0 +1,19 @@
+import requests
+
+from src.setup.base_values import BASE_URL
+from src.setup.base_values import AUTHOR_ENDPOINT
+
+AUTHOR = "My Name Is"
+
+def test_get_poems_by_author():
+    response = requests.get(f"{BASE_URL}{AUTHOR_ENDPOINT}/{AUTHOR}")
+    assert response.status_code == 200
+    error_data = response.json()
+    assert isinstance(error_data, dict)
+
+    assert "status" and "reason" in error_data
+
+    assert isinstance(error_data["status"], int)
+    assert error_data["status"] == 404
+    assert isinstance(error_data["reason"], str)
+    assert error_data["reason"] == "Not found"
